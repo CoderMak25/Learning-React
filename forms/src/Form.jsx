3afed649c94df4c523ac540f3ed59645758284f5
CoderMak25 <codewithmayankamble@gmail.com>
2026-05-02 23:54:53 +0530

    managing multiple inputs

--- forms/src/Form.jsx
+++ forms/src/Form.jsx
@@ -1,16 +1,70 @@
-import {  useState } from "react";
+import { useState } from "react";
+import "./Form.css";
+export default function Form() {
+  let [formData, setFormData] = useState({
+    fullname: "",
+    username: "",
+    password: "",
+  });
 
-export default function Form(){
-    let [username,setUsername] = useState("");
+  let changeFunc = (event) => {
+    setFormData((currData) => {
+      return { ...currData, [event.target.name]: event.target.value };
+    });
+  };
 
-    let changeFunc = (event) =>{
-        setUsername(event.target.value);
-    }
-    return (
-        <>
-        <label htmlFor="username" style={{color: "white"}}>Username</label>
-        <input id="username" type="text" placeholder="enter you name" value={username} onChange={changeFunc}/>
-        <button>Submit</button>
-        </>
-    )
+  let handleSubmit = (event) => {
+    event.preventDefault();
+    console.log(formData);
+    setFormData({
+      fullname: "",
+      username: "",
+      password: "",
+    });
+  };
+  return (
+    <form className="form" onSubmit={handleSubmit}>
+      <div>
+        <label htmlFor="fullname" style={{ color: "white" }}>
+          Fullname
+        </label>
+        <input
+          id="fullname"
+          type="text"
+          placeholder="enter you fullname"
+          value={formData.fullname}
+          onChange={changeFunc}
+          name="fullname"
+        />
+      </div>
+
+      <div>
+        <label htmlFor="username" style={{ color: "white" }}>
+          Username
+        </label>
+        <input
+          id="username"
+          type="text"
+          placeholder="enter you username"
+          value={formData.username}
+          onChange={changeFunc}
+          name="username"
+        />
+      </div>
+      <div>
+        <label htmlFor="password" style={{ color: "white" }}>
+          Password
+        </label>
+        <input
+          id="password"
+          type="password"
+          placeholder="enter you password"
+          value={formData.password}
+          onChange={changeFunc}
+          name="password"
+        />
+      </div>
+      <button>Submit</button>
+    </form>
+  );
 }
